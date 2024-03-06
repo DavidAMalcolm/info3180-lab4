9c9d35769f02f4af30ab5b8716cca858caac2966
David Malcolm <david.a.malcolms@gmail.com>
2024-03-05 23:31:39 -0500

    Updated the login route and add file upload implementation

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from flask_wtf.file import FileAllowed, FileField, FileRequired
+from wtforms import PasswordField, StringField
 from wtforms.validators import InputRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired()])
-    password = PasswordField('Password', validators=[InputRequired()])
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+
+
+class UploadForm(FlaskForm):
+    file = FileField(
+        "Please choose the file you want to upload",
+        validators=[
+            FileRequired(),
+            FileAllowed(["jpg", "png"], "Only images allowed"),
+        ],
+    )
